0b3d9cb2f91a5287cfb09d46481130a6049a9b19
JyrexRonaldo <jyrexgt38@gmail.com>
2022-10-15 17:11:51 +0000

    Improve board design with css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 const gameBoard = (function() {
-  const gameBoard = ["x", "o", "x", "o", "x", "x", "o", "x", "x"];
-
+  const gameBoard = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
   return {gameBoard}
 })();
 
@@ -8,17 +7,43 @@ const player = function(marker) {
   return {marker}
 }
 
-const game = (function() {
-  
-})()
 
 const displayController = (function(array, doc) {
+
+  const updateDisplay = () => {
+    // doc.querySelector(".shit").innerHTML = "";
     array.forEach((mark, index) => {
-        const spot = doc.createElement("div");
-        spot.setAttribute("data-index", index)
-        spot.textContent = mark;
-        doc.querySelector("body > div > div").appendChild(spot);
-    });
+      const spot = doc.createElement("div");
+      spot.setAttribute("data-index", index);
+      spot.textContent = mark;
+      doc.querySelector("body > div > div").appendChild(spot);  
+  });
+  }
+
+  const clearDisplay = () => {
+    doc.querySelector("[data-index]").textContent = "";
+  }
+
+  return {
+    updateDisplay,
+    clearDisplay
+  }
+
 })(gameBoard.gameBoard, document);
 
-// console.log(isArray(gameBoard.gameBoard))
+const game = (function(array, display, doc) {
+  display.updateDisplay()
+  
+  const spot = doc.querySelectorAll("[data-index]");
+  spot.forEach((spot) => {
+    spot.addEventListener("click", (e) => {
+      const spotIndex = e.target.getAttribute("data-index");
+      array[spotIndex] += "Doggy"
+      console.log(array)
+      display.clearDisplay()
+      display.updateDisplay()
+    })
+  })
+
+  
+})(gameBoard.gameBoard, displayController, document)
