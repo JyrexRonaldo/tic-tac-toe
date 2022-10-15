e853f6f42463ab02877117a8793b7349381ef7a1
JyrexRonaldo <jyrexgt38@gmail.com>
2022-10-15 18:56:54 +0000

    Add mark spot functionality

--- js/index.js
+++ js/index.js
@@ -1,49 +1,51 @@
-const gameBoard = (function() {
-  const gameBoard = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
-  return {gameBoard}
+const gameBoard = (function () {
+  const gameBoard = ["", "", "", "", "", "", "", "", ""];
+  return { gameBoard };
 })();
 
-const player = function(marker) {
-  return {marker}
-}
+const player = function (marker) {
+  return { marker };
+};
 
-
-const displayController = (function(array, doc) {
-
+const displayController = (function (array, doc) {
   const updateDisplay = () => {
-    // doc.querySelector(".shit").innerHTML = "";
     array.forEach((mark, index) => {
       const spot = doc.createElement("div");
       spot.setAttribute("data-index", index);
       spot.textContent = mark;
-      doc.querySelector("body > div > div").appendChild(spot);  
-  });
-  }
+      doc.querySelector("body > div:nth-child(2) > div").appendChild(spot);
+    });
+  };
 
   const clearDisplay = () => {
-    doc.querySelector("[data-index]").textContent = "";
-  }
+    doc.querySelector("body > div:nth-child(2) > div").innerHTML = "";
+  };
 
   return {
     updateDisplay,
-    clearDisplay
-  }
-
+    clearDisplay,
+  };
 })(gameBoard.gameBoard, document);
 
-const game = (function(array, display, doc) {
-  display.updateDisplay()
-  
-  const spot = doc.querySelectorAll("[data-index]");
-  spot.forEach((spot) => {
-    spot.addEventListener("click", (e) => {
-      const spotIndex = e.target.getAttribute("data-index");
-      array[spotIndex] += "Doggy"
-      console.log(array)
-      display.clearDisplay()
-      display.updateDisplay()
-    })
-  })
+const game = (function (array, display, doc) {
+  const jyrex = player("");
+  display.updateDisplay();
+  attachEvent();
 
-  
-})(gameBoard.gameBoard, displayController, document)
+  function attachEvent() {
+    const spots = doc.querySelectorAll("[data-index]");
+    spots.forEach((spot) => {
+      spot.addEventListener("click", (e) => {
+        const spotIndex = e.target.getAttribute("data-index");
+        console.log(spotIndex);
+        if (array[spotIndex] == "") {
+          array[spotIndex] = jyrex.marker;
+          display.clearDisplay();
+          console.log(array);
+          display.updateDisplay();
+          attachEvent();
+        }
+      });
+    });
+  }
+})(gameBoard.gameBoard, displayController, document);
