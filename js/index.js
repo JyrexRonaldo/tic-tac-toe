4f5021293c5b498745f2a98f00a15a3a441dd821
JyrexRonaldo <jyrexgt38@gmail.com>
2022-10-31 08:21:59 +0000

    Add form to collect and use player names

--- js/index.js
+++ js/index.js
@@ -3,23 +3,29 @@ const gameBoard = (function () {
   const clearGameBoard = () => {
     _gameBoard = ["", "", "", "", "", "", "", "", ""];
     return _gameBoard;
-  }
+  };
   const getGameBoard = () => {
     return _gameBoard;
-  }
-  return { 
+  };
+  return {
     getGameBoard,
-    clearGameBoard
-   };
+    clearGameBoard,
+  };
 })();
 
-const player = function (marker) {
-  return { marker };
+const player = function (marker, name) {
+  if (!name) {
+    name = `Player ${marker.toUpperCase()}`;
+  }
+  return {
+    name,
+    marker,
+  };
 };
 
 const displayController = (function (array, doc) {
   const updateDisplay = () => {
-    (array.getGameBoard()).forEach((mark, index) => {
+    array.getGameBoard().forEach((mark, index) => {
       const spot = doc.createElement("div");
       spot.setAttribute("data-index", index);
       spot.textContent = mark;
@@ -31,53 +37,77 @@ const displayController = (function (array, doc) {
     doc.querySelector("body > div:nth-child(2) > div").innerHTML = "";
   };
 
+  const clearPrompt = () => {
+    doc.querySelector(".prompt").style.display = "none";
+  };
+
+  const showDisplay = () => {
+    doc.querySelector(".prompt").style.display = "flex";
+  }
+
   return {
     updateDisplay,
     clearDisplay,
+    clearPrompt,
+    showDisplay
   };
 })(gameBoard, document);
 
 const game = (function (array, display, doc) {
-  const playerX = player("x");
-  const playerO = player("o");
+  let playerX = null;
+  let playerO = null;
   let currentPlayer = null;
   const restartButton = doc.querySelector("[type=button]");
   const playerPrompt = doc.querySelector("div > p");
-  let emptyStringCount = 9
+  const startButton = doc.querySelector(".prompt button");
+  let playerXName = doc.querySelector("[id=pXName]");
+  let playerOName = doc.querySelector("[id=pOName]");
+  let emptyStringCount = 9;
   display.updateDisplay();
   placeMarker();
 
+  startButton.addEventListener("click", () => {
+    setPlayerInfo();
+    playerPrompt.textContent = `${playerX.name}'s turn`;
+    display.clearPrompt();
+  });
+
+  function setPlayerInfo() {
+    playerX = playerXName.value ? player("x", playerXName.value) : player("x");
+    playerO = playerOName.value ? player("o", playerOName.value) : player("o");
+    playerXName.value = ""
+    playerOName.value = ""
+  }
+
   function switchPlayer() {
-  
     if (currentPlayer === null) {
       currentPlayer = playerX;
-      playerPrompt.textContent = "Player O's turn";
+      playerPrompt.textContent = `${playerO.name}'s turn`;
     } else if (currentPlayer === playerX) {
       currentPlayer = playerO;
-      playerPrompt.textContent = "Player X's turn";
+      playerPrompt.textContent = `${playerX.name}'s turn`;
     } else if (currentPlayer === playerO) {
       currentPlayer = playerX;
-      playerPrompt.textContent = "Player O's turn";
+      playerPrompt.textContent = `${playerO.name}'s turn`;
     }
-    return currentPlayer
+    return currentPlayer;
   }
 
   function checkWin() {
     let winner = checkPattern("x") || checkPattern("o");
-    
-    
+
     if (winner) {
       if (playerX.marker === winner.charAt(0)) {
-        playerPrompt.textContent = "Player X has won!";
+        playerPrompt.textContent = `${playerX.name} has won!`;
         endMarking();
       } else if (playerO.marker === winner.charAt(0)) {
-        playerPrompt.textContent = "Player O has won!";
+        playerPrompt.textContent = `${playerO.name} has won!`;
         endMarking();
-      } 
+      }
     }
 
     if (!winner) {
-      checkBoard()
+      checkBoard();
     }
   }
 
@@ -85,55 +115,74 @@ const game = (function (array, display, doc) {
     for (let i = 0; i < gameBoard.getGameBoard().length; i++) {
       const element = gameBoard.getGameBoard()[i];
       if (element === "") {
-        emptyStringCount--
-        return
+        emptyStringCount--;
+        return;
       }
 
       if (emptyStringCount === 0) {
-        playerPrompt.textContent = "It's a tie"
-        endMarking()
+        playerPrompt.textContent = "It's a tie";
+        endMarking();
       }
-      
     }
   }
 
   function endMarking() {
     const spots = doc.querySelectorAll("[data-index]");
     spots.forEach((spot) => {
-        spot.removeEventListener("click", attachPlacer);
+      spot.removeEventListener("click", attachPlacer);
     });
   }
 
-
-
   function checkPattern(symbol) {
     switch (true) {
-      case array.getGameBoard()[2] === `${symbol}` && array.getGameBoard()[5] === `${symbol}` && array.getGameBoard()[8] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[1] === `${symbol}` && array.getGameBoard()[4] === `${symbol}` && array.getGameBoard()[7] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[0] === `${symbol}` && array.getGameBoard()[3] === `${symbol}` && array.getGameBoard()[6] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[0] === `${symbol}` && array.getGameBoard()[1] === `${symbol}` && array.getGameBoard()[2] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[3] === `${symbol}` && array.getGameBoard()[4] === `${symbol}` && array.getGameBoard()[5] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[6] === `${symbol}` && array.getGameBoard()[7] === `${symbol}` && array.getGameBoard()[8] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[0] === `${symbol}` && array.getGameBoard()[4] === `${symbol}` && array.getGameBoard()[8] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[2] === `${symbol}` && array.getGameBoard()[4] === `${symbol}` && array.getGameBoard()[6] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[1] === `${symbol}` && array.getGameBoard()[4] === `${symbol}` && array.getGameBoard()[7] === `${symbol}`:
-        return (`${symbol} won`);
-      case array.getGameBoard()[3] === `${symbol}` && array.getGameBoard()[4] === `${symbol}` && array.getGameBoard()[5] === `${symbol}`:
-        return (`${symbol} won`);
+      case array.getGameBoard()[2] === `${symbol}` &&
+        array.getGameBoard()[5] === `${symbol}` &&
+        array.getGameBoard()[8] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[1] === `${symbol}` &&
+        array.getGameBoard()[4] === `${symbol}` &&
+        array.getGameBoard()[7] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[0] === `${symbol}` &&
+        array.getGameBoard()[3] === `${symbol}` &&
+        array.getGameBoard()[6] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[0] === `${symbol}` &&
+        array.getGameBoard()[1] === `${symbol}` &&
+        array.getGameBoard()[2] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[3] === `${symbol}` &&
+        array.getGameBoard()[4] === `${symbol}` &&
+        array.getGameBoard()[5] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[6] === `${symbol}` &&
+        array.getGameBoard()[7] === `${symbol}` &&
+        array.getGameBoard()[8] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[0] === `${symbol}` &&
+        array.getGameBoard()[4] === `${symbol}` &&
+        array.getGameBoard()[8] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[2] === `${symbol}` &&
+        array.getGameBoard()[4] === `${symbol}` &&
+        array.getGameBoard()[6] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[1] === `${symbol}` &&
+        array.getGameBoard()[4] === `${symbol}` &&
+        array.getGameBoard()[7] === `${symbol}`:
+        return `${symbol} won`;
+      case array.getGameBoard()[3] === `${symbol}` &&
+        array.getGameBoard()[4] === `${symbol}` &&
+        array.getGameBoard()[5] === `${symbol}`:
+        return `${symbol} won`;
     }
   }
 
   restartButton.addEventListener("click", (e) => {
-    playerPrompt.textContent = "Player X's turn"
-    emptyStringCount = 9
+    displayController.showDisplay()
+    setPlayerInfo()
+    playerPrompt.textContent = ``;
+    emptyStringCount = 9;
     currentPlayer = null;
     array.clearGameBoard();
     display.clearDisplay();
@@ -144,7 +193,7 @@ const game = (function (array, display, doc) {
   function placeMarker() {
     const spots = doc.querySelectorAll("[data-index]");
     spots.forEach((spot) => {
-        spot.addEventListener("click", attachPlacer);
+      spot.addEventListener("click", attachPlacer);
     });
     checkWin();
   }
@@ -158,5 +207,4 @@ const game = (function (array, display, doc) {
       placeMarker();
     }
   }
-
 })(gameBoard, displayController, document);
