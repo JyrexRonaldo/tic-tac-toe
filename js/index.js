736d65507478b0cae4474ac1fb4456e41fd09f07
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2023-10-09 14:34:23 +0000

    Add screen rendering functionality

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-function gameboard() {
-    const board = []
-}
+const gameboard = (function() {
+    const board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"] 
+
+    const getBoard = () => {
+        return board;
+    }
+    
+    return {
+        getBoard
+    }
+})()
 
 function player(marker) {
 
@@ -11,5 +19,26 @@ const gameController = (function() {
 })()
 
 const screenController = (function () {
+    const boardDiv = document.querySelector(".board");
+
+    const updateScreen = () => {
+        boardDiv.innerHTML = "";
+        const board = gameboard.getBoard()
+
+        console.log(board)
+        board.forEach((marker, index) => {
+            const markerDiv = document.createElement("button");
+            markerDiv.dataset.index = index;
+            markerDiv.textContent = marker;
+            boardDiv.appendChild(markerDiv);
+        })
+
+        boardDiv.addEventListener("click", (e) => {
+            console.log(e.target.dataset.index)
+        })
+
+    }
+
+    updateScreen()
 
 })()
