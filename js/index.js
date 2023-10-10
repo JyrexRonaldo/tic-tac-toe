1ade2c5e8614446bd8e0ed0a90c1bd8bdb7f3d0c
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2023-10-10 12:02:26 +0000

    Make game playable

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
 const gameboard = (function() {
-    // const board = [ 
-    //     "X", "O", "X", 
-    //     "O", "X", "O", 
-    //     "O", "O", "X"
-    // ] 
     const board = ["", "", "", "", "", "", "", "", ""]
     const getBoard = () => {
         return board;
@@ -28,6 +23,7 @@ function player(name, marker) {
 
 const gameController = (function() {
     const board = gameboard.getBoard()
+    let addCode = 1
     
     const players = [ 
         player("Kaiden", "X"),
@@ -36,25 +32,24 @@ const gameController = (function() {
 
     let activePlayer = players[0];
 
+    let gameStatus = `${activePlayer.name}'s turn`
+
     const switchPlayerTurn = () => {
-        console.log(activePlayer)
         activePlayer = activePlayer === players[0] ? players[1] : players[0]; 
-        console.log("yeah")
-        
     }
 
-    function checkPattern(board, marker) {
+    function getWinner(board, player) {
         switch (true) {
             case 
-            board[0] === marker && board[1] === marker && board[2] === marker ||
-            board[3] === marker && board[4] === marker && board[5] === marker ||
-            board[6] === marker && board[7] === marker && board[8] === marker ||
-            board[0] === marker && board[3] === marker && board[6] === marker ||
-            board[1] === marker && board[4] === marker && board[7] === marker ||
-            board[2] === marker && board[5] === marker && board[8] === marker ||
-            board[0] === marker && board[4] === marker && board[8] === marker ||
-            board[6] === marker && board[4] === marker && board[2] === marker :
-                return true;
+            board[0] === player.marker && board[1] === player.marker && board[2] === player.marker ||
+            board[3] === player.marker && board[4] === player.marker && board[5] === player.marker ||
+            board[6] === player.marker && board[7] === player.marker && board[8] === player.marker ||
+            board[0] === player.marker && board[3] === player.marker && board[6] === player.marker ||
+            board[1] === player.marker && board[4] === player.marker && board[7] === player.marker ||
+            board[2] === player.marker && board[5] === player.marker && board[8] === player.marker ||
+            board[0] === player.marker && board[4] === player.marker && board[8] === player.marker ||
+            board[6] === player.marker && board[4] === player.marker && board[2] === player.marker :
+                return player;
                 break;
             default:
                 return false;
@@ -62,37 +57,42 @@ const gameController = (function() {
         }
     }
 
-    console.log(checkPattern(board, "X"))
-    console.log(checkPattern(board, "O"))
-
-    function getWinner(board, player) {
-        if (checkPattern(board, player.marker) === true) {
-            return player;
-        } else {
-            return false
-        }
-    }
-
     function playRound(index) {
-        gameboard.addMarker(activePlayer.marker, index)
-
-        if (getWinner(gameboard.getBoard(), activePlayer)) {
-            console.log(`${activePlayer.name} has won `)
+        
+        if (addCode) {
+            gameboard.addMarker(activePlayer.marker, index)    
+        } 
+        
+        if (getWinner(board, activePlayer)) {
+            gameStatus = `${activePlayer.name} has won!`
+            addCode = 0;
+            return;
+        } else {
+            if (isBoardFull()) {
+                gameStatus = "It's a draw"
+            } else {
+                switchPlayerTurn()
+                gameStatus = `${activePlayer.name}'s turn`
+            }
         }
-
-        switchPlayerTurn()    
-        console.log(gameboard.getBoard())
+        console.log(isBoardFull())
     }
 
-    console.log(getWinner(board, players))
+    function isBoardFull() {
+        for (let i = 0; i < board.length; i++) {
+          if (board[i] === "") {
+            return false;
+          } 
+        }
+        return true;
+    } 
 
-    // addMarker("x", 4)
-    // addMarker("x", 5)
-    // addMarker("o", 2)
-    // addMarker("o", 1)
-    console.log(board)
-
+    function getGameStatus() {
+        return gameStatus;
+    }
+ 
     return {
+        getGameStatus,
         playRound
     }
 
@@ -100,24 +100,25 @@ const gameController = (function() {
 
 const screenController = (function () {
     const boardDiv = document.querySelector(".board");
+    const infoDiv = document.querySelector(".turn")
 
     const updateScreen = () => {
         boardDiv.textContent = "";
         const board = gameboard.getBoard()
 
-        console.log(board)
         board.forEach((marker, index) => {
             const markerDiv = document.createElement("button");
             markerDiv.dataset.index = index;
             markerDiv.textContent = marker;
             boardDiv.appendChild(markerDiv);
         })
-
-        boardDiv.addEventListener("click", (e) => {
-            console.log(e.target.dataset.index)
-        })
-
+        infoDiv.textContent = `${gameController.getGameStatus()}`
     }
+
+    boardDiv.addEventListener("click", (e) => {
+        gameController.playRound(e.target.dataset.index)
+        updateScreen()
+    })
 
     updateScreen()
 
