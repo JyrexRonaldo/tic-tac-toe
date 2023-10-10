2b906704d270c314034c805d8967cf1a963ca273
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2023-10-10 07:06:55 +0000

    Implement functionality for checking wins

--- js/index.js
+++ js/index.js
@@ -1,20 +1,79 @@
 const gameboard = (function() {
-    const board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"] 
-
+    const board = [ 
+        "X", "O", "X", 
+        "O", "X", "O", 
+        "O", "O", "X"
+    ] 
+    // const board = ["", "", "", "", "", "", "", "", ""]
     const getBoard = () => {
         return board;
     }
     
+    const addMarker = (marker, index) => {
+        board[index] = marker;
+    }
+    
     return {
-        getBoard
+        getBoard,
+        addMarker
     }
 })()
 
-function player(marker) {
-
+function player(name, marker) {
+    return {name, marker}
 }
 
 const gameController = (function() {
+    const board = gameboard.getBoard()
+    
+    const players = [ 
+        player("Kaiden", "X"),
+        player("Alenko", "O")
+    ]
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = (activePlayer === player[0]) ? players[1] : players[0]; 
+    }
+
+    function checkPattern(board, marker) {
+        switch (true) {
+            case 
+            board[0] === marker && board[1] === marker && board[2] === marker ||
+            board[3] === marker && board[4] === marker && board[5] === marker ||
+            board[6] === marker && board[7] === marker && board[8] === marker ||
+            board[0] === marker && board[3] === marker && board[6] === marker ||
+            board[1] === marker && board[4] === marker && board[7] === marker ||
+            board[2] === marker && board[5] === marker && board[8] === marker ||
+            board[0] === marker && board[4] === marker && board[8] === marker ||
+            board[6] === marker && board[4] === marker && board[2] === marker :
+                return true;
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    console.log(checkPattern(board, "X"))
+    console.log(checkPattern(board, "O"))
+
+    function getWinner(board, players) {
+        if (checkPattern(board, players[0].marker) === true) {
+            return players[0];
+        } else if (checkPattern(board, players[1].marker) === true) {
+            return players[1]
+        }
+    }
+
+    console.log(getWinner(board, players))
+
+    // addMarker("x", 4)
+    // addMarker("x", 5)
+    // addMarker("o", 2)
+    // addMarker("o", 1)
+    console.log(board)
 
 })()
 
@@ -22,7 +81,7 @@ const screenController = (function () {
     const boardDiv = document.querySelector(".board");
 
     const updateScreen = () => {
-        boardDiv.innerHTML = "";
+        boardDiv.textContent = "";
         const board = gameboard.getBoard()
 
         console.log(board)
